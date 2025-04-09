b7ae6b38ca3d207782b6a2d106a7f2950a7e841c
vals43 <hei.teddy.3@gmail.com>
2025-04-09 19:43:32 +0300

    adding progres bar accuracy for finish game

--- assets/js/graph.js
+++ assets/js/graph.js
@@ -1,63 +1,77 @@
 
 let result = localStorage.getItem('resultat');
+let inner = document.querySelector(".inner")
+let outer = document.querySelector(".outer")
+
+
 
 function createArray(str) {
-    const mots = str.split(",");
-    const array = [];
-    for (let i = 0; i < mots.length; i += 3) {
-      const sousTableau = [];
-      for (let j = 0; j < 3 && i + j < mots.length; j++) {
-        sousTableau.push(mots[i + j]);
-      }
-      array.push(sousTableau);
+  const mots = str.split(",");
+  const array = [];
+  for (let i = 0; i < mots.length; i += 3) {
+    const sousTableau = [];
+    for (let j = 0; j < 3 && i + j < mots.length; j++) {
+      sousTableau.push(mots[i + j]);
     }
-    return array;
+    array.push(sousTableau);
   }
-  const arrayResultant = createArray(result);
-  function changeStrToNumber(array) {
-    let result = array
-    for (let i = 1; i < array.length; i++) {
-        result[i][1] = Math.floor(array[i][1])
-        result[i][2] = Math.floor(array[i][2])
-    }
-    return result
-  }
-  console.log(changeStrToNumber(arrayResultant));
-  
+  return array;
+}
 
 
-          google.charts.load('current', {'packages':['corechart']});
-          google.charts.setOnLoadCallback(drawChart);
-          
-          function drawChart() {
-            var data = google.visualization.arrayToDataTable(changeStrToNumber(arrayResultant));
-    
-            var options = {
-              title: 'Result',
-              curveType: 'function',
-              legend: { position: 'bottom' },
-              colors: ['blue','red'],
-              lineWidth: 3,
-              pointSize: 10,
-              vAxis: {title: 'WPM'},
-              hAxis: {  // x-axis
-                gridlines: {color: 'FFFFFF'}
-              },
-              vAxis: {  // y-axis
-                gridlines: {color: 'FFFFFF'}
-              },
-              series: {
-                  0: {targetAxisIndex: 0}, // Series 1 on left axis
-                  1: {targetAxisIndex: 1}  // Series 2 on right axis
-              },
-              vAxes: {
-                  0: {title: 'Word per Minutes'}, // Left axis title
-                  1: {title: 'Accuracy'}  // Right axis title
-              }
-              
-            };
-    
-            var chart = new google.visualization.LineChart(document.getElementById('curve_chart'));
-    
-            chart.draw(data, options);
-          }
+const arrayResultant = createArray(result);
+function changeStrToNumber(array) {
+  let result = array
+  for (let i = 1; i < array.length; i++) {
+    result[i][1] = Math.floor(array[i][1])
+    result[i][2] = Math.floor(array[i][2])
+  }
+  return result
+}
+
+
+let accuracy = arrayResultant[arrayResultant.length - 1][2]
+
+
+inner.innerHTML = `${accuracy}%`
+
+// border of accuracy 
+
+outer.style.background = `conic-gradient(blue ${(accuracy * 360)/100}deg, rgb(255, 255, 255) 0deg)`;
+
+
+google.charts.load('current', { 'packages': ['corechart'] });
+google.charts.setOnLoadCallback(drawChart);
+
+function drawChart() {  
+  var data = google.visualization.arrayToDataTable(changeStrToNumber([arrayResultant]));
+
+  var options = {
+    title: 'Result',
+    curveType: 'function',
+    legend: { position: 'bottom' },
+    colors: ['blue', 'red'],
+    lineWidth: 3,
+    pointSize: 10,
+    vAxis: { title: 'WPM' },
+    hAxis: {  // x-axis
+      gridlines: { color: 'FFFFFF' }
+    },
+    vAxis: {  // y-axis
+      gridlines: { color: 'FFFFFF' }
+    },
+    series: {
+      0: { targetAxisIndex: 0 }, // Series 1 on left axis
+      1: { targetAxisIndex: 1 }  // Series 2 on right axis
+    },
+    vAxes: {
+      0: { title: 'Word per Minutes' }, // Left axis title
+      1: { title: 'Accuracy' }  // Right axis title
+    }
+
+  };
+
+  var chart = new google.visualization.LineChart(document.getElementById('curve_chart'));
+
+  chart.draw(data, options);
+}
