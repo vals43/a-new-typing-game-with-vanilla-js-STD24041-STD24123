0ea7f882263725f9863e6bf630ff10aed9ffeb65
vals43 <hei.teddy.3@gmail.com>
2025-04-15 19:29:25 +0300

    style: adding new color

--- assets/js/theme mode.js
+++ assets/js/theme mode.js
@@ -3,10 +3,11 @@
 let root = document.querySelector(':root');
 let theme = document.getElementById("theme")
 
-function myFunction_set(primary, secondary, tertiary) {
+function myFunction_set(primary, secondary, tertiary, quatre) {
   root.style.setProperty('--primary', primary);
   root.style.setProperty('--secondary', secondary);
   root.style.setProperty('--tertiary', tertiary);
+  root.style.setProperty('--quatre', quatre);
 }
 
 let themeConst = localStorage.getItem('theme');
@@ -19,26 +20,32 @@ if (themeConst != null) {
   
   localStorage.setItem('theme', theme.value);
   if (themeConst == 1) {
-    myFunction_set('white', 'black', '#37404F')
+    root.style.setProperty('--cinq', '--secondary');
+    myFunction_set('#FFFFF0', 'black', '#37404F', '#D9D9D9')
   }
   if (themeConst == 2) {
-    myFunction_set('black', 'white', '#BFC6D4')
+    root.style.setProperty('--cinq', '--secondary');
+    myFunction_set('#292D38', 'white', '#BFC6D4', 'black')
   }
   if (themeConst == 3) {
-    myFunction_set('#2E2F5B', '#FAF0CA', '#F4D35E')
+    root.style.setProperty('--cinq', '--secondary');
+    myFunction_set('#2E2F5B', '#FAF0CA', '#F4D35E', '#0F1C46')
   }
   if (themeConst == 4) {
-    myFunction_set('#1D2955', '#DDE8F0', '#018ABD')
+    root.style.setProperty('--cinq', '--secondary');
+    myFunction_set('#1D2955', '#DDE8F0', '#018ABD' , '#191265')
   }
   if (themeConst == 5) {
-    myFunction_set('#915A3C', '#E8E6E7', '#CFB79F')
-    
+    root.style.setProperty('--cinq', '--secondary');
+    myFunction_set('#915A3C', '#E8E6E7', '#CFB79F','#895737')
   }
   if (themeConst == 6) {
-    myFunction_set('#000', '#149414' , 'red')
+    root.style.setProperty('--cinq', '--secondary');
+    myFunction_set('#000', '#149414' , 'red' , 'black')
   }
   if (themeConst == 7) {
-    myFunction_set('#37404F', '#E0E4EB' , '#FFF')
+    root.style.setProperty('--cinq', '--secondary');
+    myFunction_set('#37404F', '#E0E4EB' , '#FFF'  , '#37404F')
   }
 }
 
@@ -48,31 +55,38 @@ theme.addEventListener('change' , () => {
   localStorage.setItem('theme', theme.value);
   
   if (theme.value == 1) {
-    myFunction_set('white', 'black', '#37404F')
+    myFunction_set('#D9D9D9', 'black', '#37404F', '#FFFFF0')
+    root.style.setProperty('--cinq', 'black');
     theme.style.background =  'linear-gradient(to right , var(--primary), var(--secondary))';
   }
   if (theme.value == 2) {
-    myFunction_set('black', 'white', '#BFC6D4')
+    myFunction_set('#292D38', 'white', '#BFC6D4','black')
+    root.style.setProperty('--cinq', '--secondary');
     theme.style.background =  'linear-gradient(to right , var(--primary), var(--secondary))';
   }
   if (theme.value == 3) {
-    myFunction_set('#2E2F5B', '#FAF0CA', '#F4D35E')
+    myFunction_set('#2E2F5B', '#FAF0CA', '#F4D35E', '#0F1C46')
+    root.style.setProperty('--cinq', '--secondary');
     theme.style.background =  'linear-gradient(to right , var(--primary), var(--secondary))';
   }
   if (theme.value == 4) {
-    myFunction_set('#1D2955', '#DDE8F0', '#018ABD')
+    myFunction_set('#1D2955', '#DDE8F0', '#018ABD' , '#191265')
+    root.style.setProperty('--cinq', '--secondary');
     theme.style.background =  'linear-gradient(to right , var(--primary), var(--secondary))';
   }
   if (theme.value == 5) {
-    myFunction_set('#915A3C', '#E8E6E7', '#CFB79F')
+    myFunction_set('#915A3C', '#E8E6E7', '#CFB79F','#895737')
+    root.style.setProperty('--cinq', '--secondary');
     theme.style.background =  'linear-gradient(to right , var(--primary), var(--secondary))';
   }
   if (theme.value == 6) {
-    myFunction_set('#000', '#149414' , 'red')
+    myFunction_set('#000', '#149414' , 'red' , 'black')
+    root.style.setProperty('--cinq', '--secondary');
     theme.style.background =  'linear-gradient(to right , var(--secondary), var(--primary))';
   }
   if (theme.value == 7) {
-    myFunction_set('#37404F', '#E0E4EB' , '#FFF')
+    myFunction_set('#37404F', '#E0E4EB' , '#FFF' , '#37404F')
+    root.style.setProperty('--cinq', '--secondary');
     theme.style.background =  'linear-gradient(to right , var(--primary), var(--secondary))';
   }
 
